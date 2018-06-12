0ab376a97ba284d96b8ea6f165c63124d59d6713
nasirbashak <nasirbashak007@gmail.com>
2018-06-12 11:53:16 +0530

    UI modified for ADD user layout and mainLayout

--- app/src/main/java/com/nasirbashak007/canteenui/MainActivity.java
+++ app/src/main/java/com/nasirbashak007/canteenui/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
