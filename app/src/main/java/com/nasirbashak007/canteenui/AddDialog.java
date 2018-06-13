40cfdbe3ebee3660c5aae307529f99184fe7f6be
Susmit-A <susmit600@gmail.com>
2018-06-13 13:45:55 +0530

    Added deduct button action.

--- app/src/main/java/com/nasirbashak007/canteenui/AddDialog.java
+++ app/src/main/java/com/nasirbashak007/canteenui/AddDialog.java
@@ -34,11 +34,13 @@ public class AddDialog extends AppCompatDialogFragment {
     private TextView toChange;
     private FirebaseObject object;
     private Context context;
+    private boolean deduct;
 
-    public AddDialog(Context c, TextView tv, FirebaseObject person){
+    public AddDialog(Context c, TextView tv, FirebaseObject person, boolean deduct){
         object = person;
         context = c;
         toChange = tv;
+        this.deduct = deduct;
     }
 
     @Override
@@ -50,56 +52,106 @@ public class AddDialog extends AppCompatDialogFragment {
 
         View view = inflater.inflate(R.layout.add_dialog_layout, null);
 
-        builder.setView(view).setTitle("Adding Credentials").setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getContext(), "Credentials Cancelled", Toast.LENGTH_SHORT).show();
+        if(!deduct) {
+            builder.setView(view).setTitle("Adding Credentials").setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(getContext(), "Credentials Cancelled", Toast.LENGTH_SHORT).show();
+                }
+            }).setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    //Name, USN, Phone, Mail
+                    //TODO: Fill these
+                    String senderID = "someone@gmail.com";
+                    String password = "********"; //TODO: Maybe set up an encrypted string, and decpypt on call
+                    String recvID = "otherperson@somemail.com";
+                    String subject = "Some Subject";
+                    String message = "Some text";
+                    //new MailSender(senderID,password).sendMailAsync(recvID,subject,message);
+
+                    final String amount = editTextAmount.getText().toString().trim();
+
+
+                    PersonDetails personDetails = new PersonDetails(amount);
+                    Toast.makeText(getContext(), amount, Toast.LENGTH_SHORT).show();
+
+                    Toast.makeText(getContext(), "From The Class " + personDetails.getAmount(), Toast.LENGTH_SHORT).show();
+
+                    //Get date and time
+                    DateFormat df = DateFormat.getDateInstance();
+                    DateFormat tf = DateFormat.getTimeInstance();
+
+                    //Update transactions list
+                    MainActivity.database.getReference().child(object.getUsn()).child("transactions").child(df.format(new Date()) + " " + tf.format(new Date())).setValue("Added " + amount, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                            final String newAmount = String.valueOf(Integer.parseInt(object.getAmount()) + Integer.parseInt(amount));
+                            MainActivity.database.getReference().child(object.getUsn()).child("amount").setValue(newAmount, new DatabaseReference.CompletionListener() {
+                                @Override
+                                public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                                    toChange.setText("Net Amount:  " + newAmount);
+                                    Toast.makeText(context, "Transaction completed successfully!", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+        else{
+            builder.setView(view).setTitle("Deducting Credentials").setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(getContext(), "Credentials Cancelled", Toast.LENGTH_SHORT).show();
+                }
+            }).setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    //Name, USN, Phone, Mail
+                    //TODO: Fill these
+                    String senderID = "someone@gmail.com";
+                    String password = "********"; //TODO: Maybe set up an encrypted string, and decpypt on call
+                    String recvID = "otherperson@somemail.com";
+                    String subject = "Some Subject";
+                    String message = "Some text";
+                    //new MailSender(senderID,password).sendMailAsync(recvID,subject,message);
+
+                    final String amount = editTextAmount.getText().toString().trim();
+
+
+                    PersonDetails personDetails = new PersonDetails(amount);
+                    Toast.makeText(getContext(), amount, Toast.LENGTH_SHORT).show();
+
+                    Toast.makeText(getContext(), "From The Class " + personDetails.getAmount(), Toast.LENGTH_SHORT).show();
+
+                    //Get date and time
+                    DateFormat df = DateFormat.getDateInstance();
+                    DateFormat tf = DateFormat.getTimeInstance();
+
+                    final String newAmount = String.valueOf(Integer.parseInt(object.getAmount()) - Integer.parseInt(amount));
+                    if(Integer.parseInt(newAmount)<0) {
+                        Toast.makeText(getContext(), "Insufficient amount", Toast.LENGTH_LONG).show();
+                        return;
                     }
-                }).setPositiveButton("ADD", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                        //Name, USN, Phone, Mail
-                        //TODO: Fill these
-                        String senderID = "someone@gmail.com";
-                        String password = "********"; //TODO: Maybe set up an encrypted string, and decpypt on call
-                        String recvID = "otherperson@somemail.com";
-                        String subject = "Some Subject";
-                        String message = "Some text";
-                        //new MailSender(senderID,password).sendMailAsync(recvID,subject,message);
-
-                        final String amount = editTextAmount.getText().toString().trim();
-
-                        
-
-
-
-
-                        PersonDetails personDetails = new PersonDetails(amount);
-                        Toast.makeText(getContext(), amount, Toast.LENGTH_SHORT).show();
-
-                        Toast.makeText(getContext(), "From The Class " + personDetails.getAmount(), Toast.LENGTH_SHORT).show();
-
-                        //Get date and time
-                        DateFormat df = DateFormat.getDateInstance();
-                        DateFormat tf = DateFormat.getTimeInstance();
-
-                        //Update transactions list
-                        MainActivity.database.getReference().child(object.getUsn()).child("transactions").child(df.format(new Date()) + " " + tf.format(new Date())).setValue("Added "+amount, new DatabaseReference.CompletionListener() {
-                            @Override
-                            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
-                                final String newAmount = String.valueOf(Integer.parseInt(object.getAmount()) + Integer.parseInt(amount));
-                                MainActivity.database.getReference().child(object.getUsn()).child("amount").setValue(newAmount, new DatabaseReference.CompletionListener() {
-                                    @Override
-                                    public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
-                                        toChange.setText("Net Amount:  "+newAmount);
-                                        Toast.makeText(context, "Transaction completed successfully!", Toast.LENGTH_LONG).show();
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
+                    //Update transactions list
+                    MainActivity.database.getReference().child(object.getUsn()).child("transactions").child(df.format(new Date()) + " " + tf.format(new Date())).setValue("Deducted " + amount, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                            MainActivity.database.getReference().child(object.getUsn()).child("amount").setValue(newAmount, new DatabaseReference.CompletionListener() {
+                                @Override
+                                public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                                    toChange.setText("Net Amount:  " + newAmount);
+                                    Toast.makeText(context, "Transaction completed successfully!", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
         editTextAmount = (EditText) view.findViewById(R.id.editTextAmount);
 
         return builder.create();
